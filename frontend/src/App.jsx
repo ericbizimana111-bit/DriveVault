50cdb22e3f20cdf4f255783a39027404f5666ebc
Bizimana Eric <ericbizimana111@gmail.com>
2026-05-29 21:58:24 +0200

    added codes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -37,6 +37,7 @@ export default function App() {
               <Route path="/contact" element={<Contact />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
+              <Route path="/verify-email" element={<VerifyEmail />} />
             </Route>
 
             {/* Driver Routes */}
