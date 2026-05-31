7d8fe835ebb76b4b5e3ad524b69cd1ae3538fd58
Bizimana Eric <ericbizimana111@gmail.com>
2026-05-31 14:12:39 +0200

    added codes

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ const csrfProtection = csurf({
   cookie: {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 3600
   }
 });
